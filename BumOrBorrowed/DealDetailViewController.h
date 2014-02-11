0f279e798278af4fb4df5dd7315a0c1f9bbb5517
jsmceo <john.s.malloy@gmail.com>
2014-02-11 17:26:13 -0600

    "second commit, with dealdetailviewcontroller"

---
+++ BumOrBorrowed/DealDetailViewController.h
@@ -0,0 +1,14 @@
+//
+//  DealDetailViewController.h
+//  BumOrBorrowed
+//
+//  Created by John Malloy on 2/11/14.
+//  Copyright (c) 2014 Big Red INC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Parse/Parse.h"
+
+@interface DealDetailViewController : UIViewController
+
+@end
---
+++ BumOrBorrowed/DealDetailViewController.m
@@ -0,0 +1,46 @@
+//
+//  DealDetailViewController.m
+//  BumOrBorrowed
+//
+//  Created by John Malloy on 2/11/14.
+//  Copyright (c) 2014 Big Red INC. All rights reserved.
+//
+
+#import "DealDetailViewController.h"
+#import "Parse/Parse.h"
+
+@interface DealDetailViewController ()
+{
+    __weak IBOutlet UITextField *lendorDealDetailViewTextField;
+    
+    __weak IBOutlet UITextField *borrowerDealDetailViewTextField;
+    
+    __weak IBOutlet UITextField *itemDealDetailViewTextField;
+    
+    __weak IBOutlet UITextField *dealNameDealDetailViewTextField;
+    
+    __weak IBOutlet UITextField *createdDateDealDetailViewTextField;
+    
+    __weak IBOutlet UITextView *descriptionDealDetailViewTextView;
+    
+    
+    
+}
+
+@end
+
+@implementation DealDetailViewController
+
+
+
+- (void)viewDidLoad
+{
+    
+    [super viewDidLoad];
+
+   //lendorDealDetailViewTextField.text =
+
+}
+
+
+@end
